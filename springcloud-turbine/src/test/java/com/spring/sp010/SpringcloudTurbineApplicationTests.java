b9d9ae8b797ce1eb8321226e69fe11451216153d
LawssssCat <1191693505@qq.com>
2020-03-06 17:59:13 +0800

    添加：turbine聚合服务

---
+++ springcloud-turbine/src/test/java/com/spring/sp010/SpringcloudTurbineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sp010;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudTurbineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
