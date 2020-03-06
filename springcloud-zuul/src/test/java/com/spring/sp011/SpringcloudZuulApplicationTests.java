9f0d10ca0e07d808241e6b444c19a30ce8549354
LawssssCat <1191693505@qq.com>
2020-03-06 19:28:03 +0800

    添加：zuul

---
+++ springcloud-zuul/src/test/java/com/spring/sp011/SpringcloudZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sp011;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
